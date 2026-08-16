5aafdc12c8e3b70c65d0c27494c433a8f03e6c72
monitus2022 <monitus202201@proton.me>
2026-08-16 15:52:30 +0800

    [AIF-C01] Added exam content json for reference

---
+++ aws_AIF_C01/src/main.py
@@ -0,0 +1,14 @@
+import instructor
+from openai import OpenAI
+import os
+from config.settings import get_openrouter_client, settings
+
+def main():
+
+    openrouter_instructor = get_openrouter_client()
+
+    
+
+
+if __name__ == "__main__":
+    main()
